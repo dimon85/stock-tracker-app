6de5f21aa5143a325dc5cbaf2f272a4331a546a5
dimon85 <glimor800@gmail.com>
2025-11-09 18:46:30 +0200

    implement search

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,8 +3,9 @@
 import Link from 'next/link';
 import { NAV_ITEMS } from '@/lib/constants';
 import { usePathname } from 'next/navigation';
+import SearchCommand from '@/components/SearchCommand';
 
-const NavItems = () => {
+const NavItems = ({ initialStocks }: { initialStocks: StockWithWatchlistStatus[] }) => {
   const pathname = usePathname();
 
   const isActive = (path: string) => {
@@ -14,16 +15,25 @@ const NavItems = () => {
   }
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-      {NAV_ITEMS.map((item) => (
-        <li key={item.label}>
-          <Link
-            href={item.href}
-            className={`hover:text-yellow-500 transition-colors ${isActive(item.href) ? 'text-gray-100' : ''}`}
-          >
-            {item.label}
-          </Link>
-        </li>
-      ))}
+      {NAV_ITEMS.map((item) => {
+        if (item.label === 'Search') {
+          return (
+            <li key="search-trigger">
+              <SearchCommand renderAs="text" label="Search" initialStocks={initialStocks} />
+            </li>
+          )
+        }
+        return (
+          <li key={item.label}>
+            <Link
+              href={item.href}
+              className={`hover:text-yellow-500 transition-colors ${isActive(item.href) ? 'text-gray-100' : ''}`}
+            >
+              {item.label}
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   )
 }
